b78843a7a24b5353789c85cb513cd2d225987784
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-07 12:12:00 -0700

    removed extra colon:

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many: :memberships, dependent: :destroy
-  has_many: :trips, through: :memberships
+  has_many :memberships, dependent: :destroy
+  has_many :trips, through: :memberships
 
 end
