9f37e0bfa0cd9592a52c0300c81c1510c6b73c6a
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-09 11:12:29 -0700

    add trip validations and methods to determine admin and members

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,7 +5,20 @@ class Trip < ApplicationRecord
   has_many :users, through: :memberships
 
   validates :address, presence: true
-
+  validates :total_seats, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1}
   geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
+  def all_members(trip)
+    trip.users = users
+  end
+
+  def member?(user)
+    user.trips.include?(self)
+  end
+
+  def admin?(user)
+    return true if self.admin == user.id
+  end
+
 end
