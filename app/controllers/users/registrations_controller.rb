fb4ee09f80d8f034385585d9833079d0e4679adc
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-09 11:13:57 -0700

    update users registration and sessions actions

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   #If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone_number, :password, :password_confirmation])
   end
 
   #If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone_number, :password, :password_confirmation, :current_password])
   end
 
   #The path used after sign up.
