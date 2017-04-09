03c982efdefbf2c08e7efe6bfdd23b4b5cf731c4
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-09 10:04:07 -0700

    add show user profile route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "trips#index"
+
+  devise_for :users, controllers: {sessions: 'users/sessions'}
+
+  get '/users/:id', to: 'users#show', as: 'user'
+
   resources :trips
   resources :memberships
+
 end
