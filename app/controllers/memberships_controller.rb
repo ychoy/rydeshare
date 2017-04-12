25640a380101636ad964a4801baab8bef6938e7a
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-11 21:33:47 -0700

    update index and create actions for membership

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,14 +1,26 @@
 class MembershipsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
     @user = current_user.user_id
     @trips = Trip.find_by_id(@user)
+    @memberships = Membership.all
   end
 
   def create
+    membership = Membership.new(membership_params)
     @trip = Trip.find(params[:trip_id])
     @trip.users.push(current_user) unless @trip.member?(current_user)
-    redirect_to trip_path(@trip)
+
+    if membership.save
+      flash[:success] = "You have joined #{membership.user.first_name}'s carpool!"
+      redirect_to trip_path(@trip)
+    else
+      flash[:error] = @membership.errors.full_messages.join(", ")
+      redirect_to root_path
+    end
+
+
   end
 
   def destroy
@@ -17,4 +29,9 @@ class MembershipsController < ApplicationController
   	redirect_to trip_path(@trip)
   end
 
+  private
+
+  def membership_params
+    params.permit(:user_id, :trip_id, :member_type, :id)
+  end
 end
