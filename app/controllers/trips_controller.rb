917aa1d70dd0392421a904ca17f389d118bf17ac
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-11 21:35:29 -0700

    incorporate memberships save, update actions to create new trips and set admin

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,8 +5,9 @@ class TripsController < ApplicationController
 
 	def index
     @trips =  Trip.all
-    @trip = Trip.new() #setting empty trips hash 
+    @trip = Trip.new() #setting empty trips hash
     @key = ENV['GOOGLE_MAPS']
+
   end
 
   def show
@@ -14,34 +15,31 @@ class TripsController < ApplicationController
   end
 
 	def new
-    @trip = Trip.new
-
+    @trip = Trip.new(user_id: params[:user_id])
+    @trips = Trip.all
 	end
 
 	def create
-		if current_user.driver? == true
-			@trip = Trip.create(trip_params)
-			@trip.admin_id = current_user.id
-
-		  if @trip.save
-				flash[:success] = "Carpool added!"
-	      redirect_to root_path
-      else
-          flash[:error] = @trip.errors.full_messages.join(", ")
-          render 'new'
-      end
-		else
-				flash[:error] = @trip.errors.full_messages.join(", ")
-	      redirect_to root_path
-	  end
-  end
+    @trip = Trip.new(trip_params)
+    @trip.user_id = current_user.id
+	  if @trip.save
+      Membership.create(user_id: current_user.id, trip_id: @trip.id, member_type: 1)
+			flash[:success] = "Carpool added!"
+      redirect_to root_path
+    else
+      flash[:error] = @trip.errors.full_messages.join(", ")
+      render 'new'
+    end
+ end
 
   def edit
-  	@trips = Trip.all
+    @trip = Trip.find(params[:id])
+    @trips = Trip.all
+    
   end
 
   def update
-    @trips = Trip.all
+    @trip = Trip.find(params[:id])
     if @trip.update(trip_params)
       redirect_to trip_path(@trip)
     else
@@ -55,7 +53,6 @@ class TripsController < ApplicationController
     redirect_to root_path
   end
 
-
   private
 
 	def set_trip
@@ -63,13 +60,12 @@ class TripsController < ApplicationController
   end
 
 	def trip_params
-    params.require(:trip).permit(:id, :address, :latitude, :longitude, :total_seats, :open_seats, :full?, :admin_id)
+    params.require(:trip).permit(:id, :address, :latitude, :longitude, :total_seats, :open_seats, :full?, :user_id)
   end
 
 	def admin_user
-		user = current_user
-		trip = Trip.find_by_id(params[:id])
-		unless trip.admin?(user)
+		@trip = Trip.find_by_id(params[:id])
+		unless @trip.admin?(current_user)
 			flash[:error] = "You must be the admin to update this carpool."
 			redirect_to trip_path(@trip)
 		end
