771eaeccc7ed6484b0f9daa636cfbaa5508a102f
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-12 12:15:21 -0700

    change memberships destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,9 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show', as: 'user'
 
   resources :trips
-  resources :memberships, only: [:destroy]
 
   get '/users/:user_id/trips', to: 'memberships#index', as: 'user_memberships'
   post '/trips/:trip_id/users', to: 'memberships#create', as: 'memberships_user'
+  delete '/trips/:trip_id/users', to: 'memberships#destroy', as: 'membership_destroy'
+
 end
