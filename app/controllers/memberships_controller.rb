4b494f748389b8e6b2c884c1837e644876dc9485
Yan Yin Choy <yanyinchoy@gmail.com>
2017-04-12 07:57:42 -0700

    fix issue with membership#create

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,35 +1,27 @@
 class MembershipsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
+  before_action :set_trip, only: [:create, :destroy]
 
   def index
-    @user = current_user.user_id
-    @trips = Trip.find_by_id(@user)
-    @memberships = Membership.all
+    @user = current_user.id
+    @trips = Trip.where(user_id:@user)
   end
 
   def create
-    membership = Membership.new(membership_params)
-    @trip = Trip.find(params[:trip_id])
     @trip.users.push(current_user) unless @trip.member?(current_user)
-
-    if membership.save
-      flash[:success] = "You have joined #{membership.user.first_name}'s carpool!"
-      redirect_to trip_path(@trip)
-    else
-      flash[:error] = @membership.errors.full_messages.join(", ")
-      redirect_to root_path
-    end
-
-
+    redirect_to trip_path(@trip)
   end
 
   def destroy
-  	@trip = Trip.find(params[:trip_id])
   	@trip.users.destroy(current_user)
   	redirect_to trip_path(@trip)
   end
 
   private
+
+  def set_trip
+    @trip = Trip.find(params[:trip_id])
+  end
 
   def membership_params
     params.permit(:user_id, :trip_id, :member_type, :id)
